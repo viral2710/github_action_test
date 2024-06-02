dae2550fd566737591010377608fe216f6363280
Viral <viral.d.aghara@gmail.com>
2024-06-02 11:03:55 +0530

    2nd commit

---
+++ function.py
@@ -0,0 +1,6 @@
+def add(a: int = 0, b: int = 0) -> int:
+    return a + b
+
+
+def sub(a: int = 0, b: int = 0) -> int:
+    return a - b
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # This is a sample Python script.
+from function import add, sub
+
 
 # Press Ctrl+F5 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -6,11 +8,13 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+    print(f"Hi, {name}")  # Press F9 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+if __name__ == "__main__":
+    print_hi("PyCharm")
+    print(add(a=10, b=5))
+    print(sub(a=10, b=5))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
